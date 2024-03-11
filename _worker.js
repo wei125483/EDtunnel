9e77ef83165d701d0faf44e2accf8623639fbf31
wei125483 <857458725@qq.com>
2024-03-11 15:38:01 +0800

    Update _worker.js
    tt

--- _worker.js
+++ _worker.js
@@ -3,7 +3,8 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+// let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '90e2725d-9bbd-4195-ac0d-e2c5829261f5';
 
 // const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.bestip.one'];
 const proxyIPs = ['cip.951535.xyz'];
